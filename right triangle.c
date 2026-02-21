d1fea2fb90e13213beeb165a80250853ca95f21a
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-02-21 19:37:57 +0530

    Add files via upload

---
+++ right triangle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int row, col;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &row);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &col);
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+
+            if (i+j >= row-1) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+    
